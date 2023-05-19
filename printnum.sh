1efe41ca83c690522e5f827f875ddbcc3f60d850
Ramesh <rameshb.dba86@gmail.com>
2023-05-19 08:10:44 +0530

    Update printnum.sh

--- printnum.sh
+++ printnum.sh
@@ -1,7 +1,9 @@
 #shell script to print numbers 1 to 100
 echo "Hello Ramesh, Good Morning!"
+hostname
+echo "this is being executed on Docker container where jenkins installed."
 i=1
-while [ $i -le 100 ]
+while [ $i -le 10 ]
 do
     echo $i
     i=$(($i+1))
